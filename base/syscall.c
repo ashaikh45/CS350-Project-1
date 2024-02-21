27b9987338cdc24b9ee4767ddf9389aa5bdc0945
ashaikh45 <ashaik5@binghamton.edu>
2024-02-21 18:20:20 -0500

    added mkdir2

--- base/syscall.c
+++ base/syscall.c
@@ -104,6 +104,8 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_shutdown(void);
+extern int sys_mkdir2(void);
+
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -128,6 +130,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_shutdown] sys_shutdown,
+[SYS_mkdir2] sys_mkdir2,
 };
 
 void
